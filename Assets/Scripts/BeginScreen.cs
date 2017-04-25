c84bb279fc6c0e65b7d00d4e468dce89a8754dcc
sarahgroell <sarah.groell@he-arc.ch>
2017-04-25 19:16:47 +0200

    first version of the begin scene

---
+++ Assets/Scripts/BeginScreen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BeginScreen : MonoBehaviour {
+
+	public void loadFirstLevel()
+    {
+        SceneManager.LoadScene("Introduction");
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+
+    public void loadBonus()
+    {
+        
+    }
+}
