df9f4c82ae90165312d279437a4a6c321074ed55
BryanMums <bryan.muhmenthaler@gmail.com>
2017-04-29 15:44:00 +0200

    Dialogue Manager + PNJ Fix #4

---
+++ Assets/Scripts/DialogueHolder.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class DialogueHolder : MonoBehaviour {
+
+    public string dialogue;
+    public Sprite icon;
+    public TextAsset textFile;
+    private DialogueManager dMan;
+
+	// Use this for initialization
+	void Start () {
+        dMan = FindObjectOfType<DialogueManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Debug.Log("LOL");
+            if(Input.GetKeyUp(KeyCode.G))
+            {
+                Debug.Log("Enter");
+                string texts = textFile.text;
+                dMan.ShowBox(texts, icon);
+            }
+        }
+    }
+}
+
---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour {
+
+    public GameObject dBox;
+    public Text dText;
+    private string[] listSentences;
+    private int length;
+    private int current;
+
+    public bool dialogActive;
+
+	// Use this for initialization
+	void Start () {
+        dialogActive = false;
+        dBox.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(dialogActive && Input.GetKeyDown(KeyCode.E))
+        {
+            //Il faut d'abord voir s'il reste du texte
+            if(current < length - 1)
+            {
+                current++;
+                SetText();
+            }else
+            {
+                //Sinon on enlève la box.
+                dBox.SetActive(false);
+                dialogActive = false;
+            }
+        }
+	}
+
+    public void ShowBox(string dialogue, Sprite icon)
+    {
+        
+        dialogActive = true;
+        dBox.SetActive(true);
+
+        GameObject imageObject = GameObject.FindGameObjectWithTag("IconDialogBox");
+
+        if (imageObject != null)
+        {
+            Image iconImage = imageObject.GetComponent<Image>();
+            iconImage.sprite = icon;
+        }
+
+        listSentences = dialogue.Split('\n');
+        length = listSentences.Length;
+        current = 0;
+        SetText();
+    } 
+
+    public void SetText()
+    {
+        dText.text = listSentences[current];
+    }
+}
